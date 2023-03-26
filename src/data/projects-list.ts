61545397e7cf1b57752acc1be3150504db93a004
albertojoao <allbertto.joao@gmail.com>
2023-03-26 18:01:45 -0300

    updates

--- src/data/projects-list.ts
+++ src/data/projects-list.ts
@@ -1,12 +1,11 @@
-import peterpitch from "assets/images/projects/pp.png";
+
 
 export const projectsList = [
   {
     id: 1,
     title: "Building - In Progress",
-    logo: peterpitch,
     blurHash: "L4ADc400P*Zi4Tu1y;Qo00pH#YXl",
-    desc: `Peterpitch is a tool that takes the fun in your company seriously and turns your meetings into real performance boosters. With Peterpitch, you round up the pack of employees and turn even the shakiest puppies into true wolves.`,
+    desc: `Building - In Progress`,
     technologies: ["rails", "react", "javascript"]
   },
  
